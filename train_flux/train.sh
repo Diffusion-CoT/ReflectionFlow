6d5aa7c0361bec8dfee0ea0c75282b12809fe1be
Le Zhuo <53815869+zhuole1025@users.noreply.github.com>
2025-04-21 23:20:02 +0800

    Update train.sh

--- train_flux/train.sh
+++ train_flux/train.sh
@@ -4,7 +4,7 @@
 export XFL_CONFIG=config.yaml
 
 # Specify the WANDB API key
-export WANDB_API_KEY="6259cb5e7b3f9ef85de258325ba564e92827f2c5"
+export WANDB_API_KEY=""
 
 export TOKENIZERS_PARALLELISM=true
 accelerate launch --main_process_port 41353 -m train.train
